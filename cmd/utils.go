dcb5c53b09fcef2db51a15aa4423df9fbdd6a439
Liam Hampton <liam.hampton@hotmail.co.uk>
2024-07-12 23:50:27 +0100

    remove printline from user input util function

--- cmd/utils.go
+++ cmd/utils.go
@@ -8,7 +8,6 @@ import (
 
 func GetUserInput(userPrint string) string {
 	reader := bufio.NewReader(os.Stdin)
-	// fmt.Print("I am a generic helper. I may or may not know the answer to your question so be patient with me. \nPlease enter your question: ")
 	fmt.Print(userPrint)
 	userInput, _ := reader.ReadString('\n')
 	return userInput
