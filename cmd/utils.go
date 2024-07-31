036342c41c9d2f7d79940d870b767e56a975a268
Liam Hampton <liam.hampton@hotmail.co.uk>
2024-07-31 21:24:37 +0100

    add survey for user to select differnt local models

--- cmd/utils.go
+++ cmd/utils.go
@@ -4,6 +4,8 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+
+	"github.com/AlecAivazis/survey/v2"
 )
 
 func GetUserInput(userPrint string) string {
@@ -11,4 +13,22 @@ func GetUserInput(userPrint string) string {
 	fmt.Print(userPrint)
 	userInput, _ := reader.ReadString('\n')
 	return userInput
+}
+
+func GetLocalModel() string {
+	options := []string{"llama3.1", "phi3", "mistral"}
+
+	var selectedOption string
+	prompt := &survey.Select{
+		Message: "Choose a local model to use (you must have it installed!):",
+		Options: options,
+	}
+	err := survey.AskOne(prompt, &selectedOption)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error selecting local model: %v\n", err)
+		return ""
+	}
+
+	fmt.Printf("You selected: %s\n", selectedOption)
+	return selectedOption
 }
